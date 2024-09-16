589ab9b7c43b06bfddb0da6a3ccffa48e6f700e8
Instalacao-A7 <121968281+Instalacao-A7@users.noreply.github.com>
2024-09-16 14:50:34 -0300

    Update integracao_procfit_cosmos.sh

--- integracao_procfit_cosmos.sh
+++ integracao_procfit_cosmos.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Variável de exemplo
+TipoServidor=`hostname | cut -b 1-9`
 
 echo "########################################################"
 echo " 		    Iniciando Procedimentos                   "
@@ -93,6 +95,27 @@ psql -X -h $END_SERVIDOR -U postgres -d $CHINCHILA_DS_DATABASENAME --command="$G
 service postgresql-$PG_VERSION reload
 
 psql -X -h $END_SERVIDOR -U "integracao_procfit_cosmos" -d $CHINCHILA_DS_DATABASENAME --password --command="$QueryTeste"
+
+#/ Condição no if
+if [ $TipoServidor == "localhost" ]; then
+        echo "#######################################################"
+	echo ""
+  	echo "O servidor dessa loja é físico, não é necessário "
+	echo "realizar a liberação do firewall."
+        echo "#######################################################"
+
+
+
+else
+        echo "#######################################################"
+	echo ""
+	echo "O servidor dessa loja é em nuvem, seguir os passos do  "
+        echo " 	  KB http://kb.a7.net.br/index.php?curid=9470"
+        echo "#######################################################"
+
+	echo ""
+	echo Pressione qualquer tecla para prosseguir!	
+	read ciente
 
 echo " "
 echo "#########################################################"
